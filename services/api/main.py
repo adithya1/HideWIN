41ba5b7a628f93eb61d1bd0e76b30deac80ec3e2
Aditya Win <aditya-win@users.noreply.github.com>
2026-09-07 00:31:56 -0500

    feat(auth): add signup and login HTTP endpoints

--- services/api/main.py
+++ services/api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from pydantic import ValidationError
 from services.api.core.config import settings
-from services.api.routers import admin
+from services.api.routers import admin, auth
 from services.api.core.exceptions import ApplicationError, application_error_handler, validation_error_handler
 
 app = FastAPI(title="Hide-WIN API", version="1.0.0", debug=settings.DEBUG)
-app.include_router(admin.router)
+app.include_router(admin.router)\napp.include_router(auth.router)
 
 app.add_exception_handler(ApplicationError, application_error_handler)
 app.add_exception_handler(ValidationError, validation_error_handler)
