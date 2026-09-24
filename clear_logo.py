30ddbff01a44b8a0172c938937bb6776a94f699a
Aditya Win <aditya-win@users.noreply.github.com>
2026-09-23 23:59:40 -0500

    chore: remove unnecessary patch and fix script files from root

--- clear_logo.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('services/api/data/admin_settings.json', 'r') as f:
-    data = json.load(f)
-data['logo_light_url'] = ''
-data['logo_dark_url'] = ''
-with open('services/api/data/admin_settings.json', 'w') as f:
-    json.dump(data, f)
